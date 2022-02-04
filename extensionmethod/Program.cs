af841f39aa0cb687ef541cf296dc0a725aba0560
Jahnavi0705 <pgs19012_mca.jahnavi@cbit.org.in>
2022-02-04 16:20:32 +0530

    sixth

---
+++ extensionmethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace extensionmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sealed class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace sealed_class
+{
+
+    class Program
+    {
+        static public void sampleMethod(dynamic val)
+        {
+            Console.WriteLine(val.s_id);
+            Console.WriteLine(val.s_name);
+            Console.WriteLine(val.language);
+            Console.WriteLine(val.nested_ob.email);
+        }
+        public static void print(int s)
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            var anony_Object = new
+            {
+                s_id = 123,
+                s_name = "Jahnavi",
+                language = "English",
+                nested_ob = new { email = "Jahnavi@gmail.com" }
+            };
+            sampleMethod(anony_Object);
+        }
+    }
+}
